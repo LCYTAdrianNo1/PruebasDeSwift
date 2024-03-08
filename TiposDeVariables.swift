fe853d3579047a6a7f4ac8f1c58d79c9b18f7f67
LCYTAdrianNo1 <80604147+LCYTAdrianNo1@users.noreply.github.com>
2024-03-07 23:04:41 -0500

    V1.0.070324
    -Tipos de variables en Swift

---
+++ TiposDeVariables.swift
@@ -0,0 +1,17 @@
+// Variables de diferentes tipos en Swift
+let cadena: String = "Adrian"
+let entero: Int = 21
+let flotante: Float = 3.14
+let caracter: Character = "A"
+let cadenaCaracteres: String = "Texto variable"
+let booleano: Bool = true
+let array: [Int] = [1, 2, 3, 4, 5]
+
+// Imprimir variables
+print("Cadena: \(cadena)")
+print("Entero: \(entero)")
+print("Flotante: \(flotante)")
+print("Caracter: \(caracter)")
+print("Cadena de Caracteres: \(cadenaCaracteres)")
+print("Booleano: \(booleano)")
+print("Array: \(array)")
